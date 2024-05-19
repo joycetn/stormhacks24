bbea1966e06c0e18889651f07a48133b84379ad8
JeniseCheung <124667789+JeniseCheung@users.noreply.github.com>
2024-05-18 23:44:40 -0700

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Stormhacks2024"
+include(":app")
+ 
